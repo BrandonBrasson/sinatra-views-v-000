6e5f4fdd4fb0f07d83efb8265c53603b9b5463a1
Brandon Brasson <rocksong622@gmail.com>
2019-12-03 20:15:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,33 +7,7 @@ class App < Sinatra::Base
 			erb :index
 		end
 
-	get '/info' do
-	     erb :info
-	  end
-
-		get '/' do
-			"<h1>Hello World</h1>"
-		end
-
-		get '/' do
-			erb :index
-		end
-
-		get '/' do
-			erb :index
-		end
-
-		get "/info" do
-			"Testing the info page"
-		end
-
-		get '/' do
-			erb :index
-		end
-
-		get "/info" do
-			erb :info
-		end
+	
 
 		get '/' do
 			erb :index
