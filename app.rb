a116419c645418338f2f39c4e20b95295cea7d97
Brandon Brasson <rocksong622@gmail.com>
2019-12-03 20:11:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,4 +11,23 @@ class App < Sinatra::Base
 	     erb :info
 	  end
 
+		get '/' do
+			"<h1>Hello World</h1>"
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get "/info" do
+			"Testing the info page"
+		end
+
+
+
+
 end
