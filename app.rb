ab0ff41aa5f8c641b5d4b8dbde4ba445a8827db3
Brandon Brasson <rocksong622@gmail.com>
2019-12-03 20:19:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,38 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 
+	get '/' do
+			erb :index
+		end
 
+	get '/info' do
+	     erb :info
+	  end
 
-	
+		get '/' do
+			"<h1>Hello World</h1>"
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get "/info" do
+			"Testing the info page"
+		end
+
+		get '/' do
+			erb :index
+		end
+
+		get "/info" do
+			erb :info
+		end
+
 		get '/' do
 			erb :index
 		end
